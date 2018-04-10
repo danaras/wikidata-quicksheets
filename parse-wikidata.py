5ccec81df37fc1e6f82cbc8be2805ca9be5e5e7e
Danara Sarioglu <ds4489@nyu.edu>
2018-04-10 16:03:28 -0400

    added a function to deal with redirects

--- parse-wikidata.py
+++ parse-wikidata.py
@@ -4,7 +4,7 @@ import re
 from QIDfromCategories import getQidFromCategories
 from findOccupation import findOccupationFirstSentence
 
-inputFileName = 'output-improved'
+inputFileName = 'pre2017-improved'
 matrixName = '_QID_matrix_output_mar23.csv'
 matrixGrepName = '_QID_matrixGREP_output_mar23.csv'
 grep = True
@@ -19,6 +19,43 @@ gender = ''
 occupation = ''
 firstline = True
 
+def getWikiData(language, title):
+	request = Request('https://www.wikidata.org/w/api.php?action=wbgetentities&sites='+language+'wiki&titles='+title+'&languages='+language+'&props=claims%7Clabels&format=json')
+	try:
+		response = urlopen(request, timeout=5)
+		wikiData = response.read()
+		jsonData = json.loads(wikiData)
+		return jsonData
+	except:
+	    print 'General error. Got an error code:'
+
+def getQID(jsonObject):
+	try:
+		keys = jsonObject["entities"].keys()
+		entitiesFound = True
+		for key in keys:
+			_qid = key
+		print _qid
+		return _qid
+	except:
+		print "cannot find entities"
+
+
+def getRedirect(language, titleWP):
+	redirect = Request('https://'+language+'.wikipedia.org/w/api.php?action=query&titles='+titleWP+'&redirects=yes&format=json')
+	try:
+		responsePWRedirect = urlopen(redirect, timeout=5)
+		wikiPediaRedirect = responsePWRedirect.read()
+		jsonDataPWRedirect = json.loads(wikiPediaRedirect)
+		try:
+			titleRedirect = jsonDataPWRedirect["query"]["redirects"][0]["to"]
+			return titleRedirect.encode('utf8')
+		except:
+			print "redirect title couldn't be found"
+			return titleWP.replace('%20', ' ')
+	except:
+		print "something went wrong getting the redirect title page"
+		return titleWP.replace('%20', ' ')
 if not os.path.exists(inputFileName+" Outputs"):
 	os.makedirs(inputFileName+" Outputs")
 if not os.path.exists(inputFileName+" Outputs/already has P106"):
@@ -37,6 +74,14 @@ csvWriterFemaleLack.writerow(['language','title','QID','p21','gender','p106','oc
 outputOther = open(inputFileName+' Outputs/needs human review/output-other.csv', 'w')
 csvWriterOther = csv.writer(outputOther)
 csvWriterOther.writerow(['language','title','QID','p21','gender','p106','occupation','pw first sentence'])
+#output file for has wikipedia but no wikidata
+outputOtherNoWD = open(inputFileName+' Outputs/needs human review/output-hasWP-noWD.csv', 'w')
+csvWriterOtherNoWD = csv.writer(outputOtherNoWD)
+csvWriterOtherNoWD.writerow(['language','title','QID','p21','gender','p106','occupation','pw first sentence'])
+#output file for likely deleted
+outputOtherDeleted = open(inputFileName+' Outputs/needs human review/output-likelyDeleted.csv', 'w')
+csvWriterOtherDeleted = csv.writer(outputOtherDeleted)
+csvWriterOtherDeleted.writerow(['language','title','QID','p21','gender','p106','occupation','pw first sentence'])
 
 
 with open(inputFileName+'.csv','rb') as csvfile:
@@ -47,16 +92,34 @@ with open(inputFileName+'.csv','rb') as csvfile:
 			continue
 		info = list(row)
 		# langTitle = info[0]
-		language = info[0]
+		try:
+			language = info[0]
+		except:
+			print "no lang found"
 		# titleOriginal = langTitle.split(':')[1]
-		titleOriginal = info[1]
-		title = titleOriginal.replace(' ', '+')
-		print language
-		print title
-		titleWP=titleOriginal.replace(' ', '%20')
-		print titleWP
-		request = Request('https://www.wikidata.org/w/api.php?action=wbgetentities&sites='+language+'wiki&titles='+title+'&languages='+language+'&props=claims%7Clabels&format=json')
+		try:
+			titleOriginal = info[1]
+			title = titleOriginal.replace(' ', '+')
+			print language
+			print title
+			titleWP=titleOriginal.replace(' ', '%20')
+			print titleWP
+		except:
+			print "title couldn't be read from input file"
+		jsonData = getWikiData(language, title)
+		if jsonData:
+			qid =getQID(jsonData)
+		if qid:
+			if str(qid) == "-1":
+				titleOriginal = getRedirect(language, titleWP)
+				if titleOriginal:
+					titleWP = titleOriginal.replace(' ', '%20')
+					title =  titleOriginal.replace(' ', '+')
+					jsonData = getWikiData(language, title)
+					qid = getQID(jsonData)
+
 		wpRequest = Request('https://'+language+'.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro=&explaintext=&titles='+titleWP)
+		#https://en.wikipedia.org/w/api.php?action=query&titles=Elodie%20Courter&redirects=yes
 		try:
 			responsePW = urlopen(wpRequest, timeout=5)
 			wikiPedia = responsePW.read()
@@ -65,27 +128,13 @@ with open(inputFileName+'.csv','rb') as csvfile:
 				keys = jsonDataPW["query"]["pages"].keys()
 				extract = jsonDataPW["query"]["pages"][keys[0]]["extract"]
 				print extract
-				firstSentence = re.search(r'^.*?\w\w+\.', extract).group(0).encode("utf8")
+				firstSentence = re.search(r'^.*?\w\w+\)?\.', extract).group(0).encode("utf8")
 
 				print firstSentence
 			except:
 				print "cannot find extract"
 		except:
 			print "erroor"
-		try:
-			response = urlopen(request, timeout=5)
-			wikiData = response.read()
-			jsonData = json.loads(wikiData)
-		except:
-		    print 'General error. Got an error code:'
-		try:
-			keys = jsonData["entities"].keys()
-			entitiesFound = True
-		except:
-			print "cannot find entities"
-		for key in keys:
-			qid = key
-		print qid
 		try:
 			p21 = (jsonData["entities"][qid]["claims"]["P21"][0]["mainsnak"]["datavalue"]["value"]["id"])
 			print jsonData["entities"][qid]["claims"]["P21"][0]["mainsnak"]["datavalue"]["value"]["id"]
@@ -140,8 +189,16 @@ with open(inputFileName+'.csv','rb') as csvfile:
 							csvWriterFemaleLack.writerow([language, titleOriginal, qid, p21, gender, p106, occupation, firstSentence])
 							outputFemaleLack.flush()
 		else:
-			csvWriterOther.writerow([language, titleOriginal, qid, p21, gender, p106, occupation, firstSentence])
-			outputOther.flush()
+			if str(qid) == "-1":
+				if firstSentence:
+					csvWriterOtherNoWD.writerow([language, titleOriginal, qid, p21, gender, p106, occupation, firstSentence])
+					outputOtherNoWD.flush()
+				else:
+					csvWriterOtherDeleted.writerow([language, titleOriginal, qid, p21, gender, p106, occupation, firstSentence])
+					outputOtherDeleted.flush()
+			else:
+				csvWriterOther.writerow([language, titleOriginal, qid, p21, gender, p106, occupation, firstSentence])
+				outputOther.flush()
 
 		keys = []
 		qid = ''
@@ -152,3 +209,4 @@ with open(inputFileName+'.csv','rb') as csvfile:
 		gender = ''
 		occupation = ''
 		firstSentence = ''
+		jsonData = ''
