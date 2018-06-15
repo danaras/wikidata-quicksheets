f015237866f5e662fe5c2984f4d467a6c55899b4
Danara Sarioglu <ds4489@nyu.edu>
2018-06-15 16:04:34 -0400

    generalized quick statement class

--- quick-statement.py
+++ quick-statement.py
@@ -1,76 +1,92 @@
 import csv
 # from fuzzywuzzy import fuzz
 from settings import SettingsForQS
+from variables import *
 
 setup = SettingsForQS()
 wikipedias = setup.wikipediaOptions
 minCount = setup.minCount
 popularCount = setup.popularCountSteps
-# accuracy = setup.accuracy
-name = 'pre2017 new'
-firstlineOccupation = True
-firstlineALT = True
-occupations = []
-wpEn = "Q328"
-wikipediaQID = ''
-referencedIn = "S143"
-propertyId = "P106"
-outputCSV = open(name+'-output-qs.csv', 'w')
-csvWriter = csv.writer(outputCSV)
-csvWriter.writerow(['QID','property id','occupationQID','referenced in','wikipediaQID'])
 
-outputCSVALT = open(name+'-output-altOccupation-accuracy100.csv', 'w')
-csvWriterALT = csv.writer(outputCSVALT)
-csvWriterALT.writerow(['language','title','QID','p21','gender','p106','occupation description','popular','accept value','occupation','alt occupation','pw first sentence'])
+class QuickStatement:
+	def __init__(self):
+		self.name = inputFileNameQuickStatements
+		self.pValueList = []
+	def openCSVtoWrite(self):
+		if nonWikiRef:
+			self.outputCSVRef = open(self.name+'-output-ref-qs.csv', 'w')
+			self.csvWriterRef = csv.writer(self.outputCSVRef)
+			self.csvWriterRef.writerow(rowQSRef)
+		else:
+			self.outputCSV = open(self.name+'-output-qs.csv', 'w')
+			self.csvWriter = csv.writer(self.outputCSV)
+			self.csvWriter.writerow(rowQSallWP)
 
-outputCSVNEW = open(name+'-output-NEWOccupation.csv', 'w')
-csvWriterNEW = csv.writer(outputCSVNEW)
-csvWriterNEW.writerow(['language','title','QID','p21','gender','p106','occupation description','popular','accept value','occupation','alt occupation','pw first sentence'])
+			self.outputCSVALT = open(self.name+'-output-alt'+pValue[1][1]+'.csv', 'w')
+			self.csvWriterALT = csv.writer(self.outputCSVALT)
+			self.csvWriterALT.writerow(rowEdit)
 
-with open('occupations-withDescriptions.csv', 'r') as f:
-	reader = csv.reader(f)
-	for line in reader:
-		if firstlineOccupation:    #skip first line
-			firstlineOccupation = False
-			continue
-		info = list(line)
-		if int(info[3]) >= minCount:
-			if int(info[3]) >= popularCount[0]:
-				popular = '*'
-			elif int(info[3]) >= popularCount[1]:
-				popular = '.'
-			else:
-				popular = ' '
-			qidLink = info[0].split('/')
-			qid = qidLink[len(qidLink)-1]
-			occupations.append([info[1],qid,popular,info[2]])
+			self.outputCSVNEW = open(self.name+'-output-NEW'+pValue[1][1]+'.csv', 'w')
+			self.csvWriterNEW = csv.writer(self.outputCSVNEW)
+			self.csvWriterNEW.writerow(rowEdit)
+
+	def preparePValueList(self):
+		with open(pValueListName+'.csv', 'r') as f:
+			firstline = True
+			reader = csv.reader(f)
+			for line in reader:
+				if firstline:    #skip first line
+					firstline = False
+					continue
+				info = list(line)
+				if int(info[3]) >= minCount:
+					if int(info[3]) >= popularCount[0]:
+						popular = '*'
+					elif int(info[3]) >= popularCount[1]:
+						popular = '.'
+					else:
+						popular = ' '
+					qidLink = info[0].split('/')
+					qid = qidLink[len(qidLink)-1]
+					self.pValueList.append([info[1],qid,popular,info[2]])
 #if it throws an error saying "_csv.Error: new-line character seen in unquoted field -
 #do you need to open the file in universal-newline mode?"
 #then you should save the document as plan text csv file again
-with open('AF WD Found Occupation for verification - pre2017 new.csv','r') as csvFile:
-	reader = csv.reader(csvFile)
-	for row in reader:
-		if firstlineALT:    #skip first line
-			firstlineALT = False
-			continue
-		info = list(row)
-		if info[8]:
-			if "new" in info[8]:
-				print "new found"
-				csvWriterNEW.writerow([info[0],info[1],info[2],info[3],info[4],"",x[3],x[2],"",occupation,"",info[11]])
-			else:
-				for x in wikipedias:
-					if x[0] in info[0]:
-						wikipediaQID = x[1]
-					else:
-						wikipediaQID = wpEn
-				csvWriter.writerow([info[2], propertyId, info[5], referencedIn, wikipediaQID])
-		if info[10]:
-			for index, x in enumerate(occupations):
-				if x[0] == info[10]:
-					occupation = x[0]
-					occupationQID = x[1]
-					csvWriterALT.writerow([info[0],info[1],info[2],info[3],info[4],occupationQID,x[3],x[2],"",occupation,info[10],info[11]])
-					csvWriter.writerow([info[2], propertyId, occupationQID, referencedIn, wikipediaQID])
+	def parseInputFile(self):
+		with open(self.name+'.csv','r') as csvFile:
+			firstline = True
+			reader = csv.reader(csvFile)
+			for row in reader:
+				if firstline:    #skip first line
+					firstline = False
+					continue
+				info = list(row)
+				if nonWikiRef:
+					if info[7]:
+						if 'y' == info[7]:
+							csvWriterRef.writerow([])
+				else:
+					if info[8]:
+						if "new" in info[8]:
+							print "new found"
+							csvWriterNEW.writerow([info[0],info[1],info[2],info[3],info[4],"",x[3],x[2],"",info[10],"",info[11]])
+						else:
+							for x in wikipedias:
+								if x[0] in info[0]:
+									wikipediaQID = x[1]
+								else:
+									wikipediaQID = wpEn
+							csvWriter.writerow([info[2], propertyId, info[5], referencedIn, wikipediaQID])
+					if info[10]:
+						self.preparePValueList()
+						for index, x in enumerate(self.pValueList):
+							if x[0] == info[10]:
+								p2Value = x[0]
+								p2QID = x[1]
+								csvWriterALT.writerow([info[0],info[1],info[2],info[3],info[4],p2QID,x[3],x[2],"",p2Value,info[10],info[11]])
+								csvWriter.writerow([info[2], propertyId, p2QID, referencedIn, wikipediaQID])
 
-		wikipediaQID = ''
+					wikipediaQID = ''
+
+QS = QuickStatement()
+QS.parseInputFile()
--- variables.py
+++ variables.py
@@ -1,25 +1,40 @@
+# Variables both for quicksheets.py and quick-statement.py ##############
+pValueListName = 'pList'
 debug = False
-useCategories = False
-useFirstSentence = False
 getReferences = True
-refLinkLimit = 5
+rowQS = ['QID of person', pValues[1][0], 'QID of '+pValues[1][1], 'stated in', 'enwiki']
+rowEdit = ['language','title','QID',pValues[0][0],pValues[0][1],pValues[1][0],'popular','accept value',pValues[1][1],pValues[1][1]+' description','alt '+pValues[1][1],'WP first sentence']
+firstline = True
+
+# Variables only for quicksheets.py
+inputFileNameQuickSheets = 'testList'
 pValues = [['P21','gender'],['P172','ethnic group']]
 genderSelect = ["male","female","transgender female","transgender male","non-binary","intersex","transgender","hermaphrodite","neutral sex"] #choose from 'female', 'male', etc.
-rowEdit = ['language','title','QID',pValues[0][0],pValues[0][1],pValues[1][0],'popular','accept value',pValues[1][1],pValues[1][1]+' description','alt '+pValues[1][1],'WP first sentence']
-rowHuman = ['language','title','QID',pValues[0][0],pValues[0][1],pValues[1][0],pValues[1][1],'WP first sentence']
-rowQS = ['QID of person', pValues[1][0], 'QID of '+pValues[1][1], 'stated in', 'enwiki']
-rowRef = ['language','title','QID',pValues[0][0],pValues[0][1],pValues[1][0],pValues[1][1],'accept value','Reference Link', 'context']
-inputFileName = 'testList'
+
+useCategories = False
 matrixName = '_QID_matrix_output_mar23.csv'
-matrixGrepName = '_QID_matrixGREP_output_mar23.csv'
 grep = True
+matrixGrepName = '_QID_matrixGREP_output_mar23.csv'
+
+useFirstSentence = False
+firstSentence = ''
+
+refLinkLimit = 5
+rowRef = ['language','title','QID',pValues[0][0],pValues[0][1],pValues[1][0],pValues[1][1],'accept value','Reference Link', 'context']
+
+rowHuman = ['language','title','QID',pValues[0][0],pValues[0][1],pValues[1][0],pValues[1][1],'WP first sentence']
+
 title =''
 language =''
 qid=''
 entitiesFound = False
-firstSentence = ''
-p21 = ''
-p106 = ''
-gender = ''
-occupation = ''
-firstline = True
+
+# Variables only for quick-statement.py #####################################
+nonWikiRef = True
+inputFileNameQuickStatements = 'output-references'
+rowQSallWP = ['QID','property id','occupationQID','referenced in','wikipediaQID']
+rowQSRef = []
+wpEn = "Q328"
+wikipediaQID = ''
+referencedIn = "S143"
+propertyId = "P106"
